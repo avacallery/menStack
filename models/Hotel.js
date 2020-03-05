ad33449528972de0232239ff0cdac5ad9aec14f1
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-03-05 12:31:07 -0500

    made schema for hotel room

--- models/Hotel.js
+++ models/Hotel.js
@@ -1,3 +1,25 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
+const Hotel = mongoose.model('Hotel', new mongoose.Schema({
+    checkIn: {
+        type: Date,
+        required: true
+    },
+    checkOut: {
+        type: Date,
+        required: true
+    },
+    numberOfNights: {
+        type: Number,
+        required: true
+    },
+    adults: {
+        type: Number,
+        required: true
+    },
+    children: {
+        type: Number,
+        required: false
+    }
+}));
