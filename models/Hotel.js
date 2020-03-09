d1286ddb3023a0e9b14abd3d6f0bc6664efd3e02
avacallery <avacallery@gmail.com>
2020-03-09 14:28:46 -0400

    display object id of Hotel.rooms in console (empty array)

--- models/Hotel.js
+++ models/Hotel.js
@@ -2,28 +2,28 @@ const Joi = require('joi');
 const mongoose = require('mongoose');
 
 const Hotel = mongoose.model('Hotel', new mongoose.Schema({
-    checkIn: {
-        type: Date,
-        required: true
-    },
-    checkOut: {
-        type: Date,
-        required: true
-    },
-    numberOfNights: {
-        type: Number,
-        required: false
-    },
-    adults: {
-        type: Number,
-        required: true
-    },
-    children: {
-        type: Number,
-        required: false
-    },
+    // checkIn: {
+    //     type: Date,
+    //     required: true
+    // },
+    // checkOut: {
+    //     type: Date,
+    //     required: true
+    // },
+    // numberOfNights: {
+    //     type: Number,
+    //     required: false
+    // },
+    // adults: {
+    //     type: Number,
+    //     required: true
+    // },
+    // children: {
+    //     type: Number,
+    //     required: false
+    // },
     rooms: {
-        type: String + Array,
+        type: Array,
         roomsByType: [
             {roomType: 'Deluxe Suite', roomNumbers: [101, 103, 106, 109, 112]},
             {roomType: 'Executive Suite', roomNumbers: [202, 204, 206, 208, 210]},
@@ -36,11 +36,11 @@ const Hotel = mongoose.model('Hotel', new mongoose.Schema({
 function validateHotelRooms(hotel) {
 
     const schema = {
-        checkIn:Joi.Date().required(),
-        checkOut:Joi.Date().required(),
-        numberOfNights:Joi.Number().required(false),
-        adults:Joi.Number().required(),
-        children:Joi.Number().required(false),
+        // checkIn:Joi.Date().required(),
+        // checkOut:Joi.Date().required(),
+        // numberOfNights:Joi.Number().required(false),
+        // // adults:Joi.Number().required(),
+        // // children:Joi.Number().required(false),
         rooms:Joi.String().Array().required()
 
     };
--- routes/api/hotels.js
+++ routes/api/hotels.js
@@ -1,15 +1,18 @@
-const { hotelSchema, validate } = require('../models/Hotel');
+const { Hotel, validate } = require('../../models/Hotel');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send({msg: "Works"});
-});
+// router.get('/', (req, res) => {
+//     res.send({msg: "Works"});
+// });
 
 router.get('/', async(req, res) => {
-    const leHotel = await hotelSchema.find().sort('roomsBytype');
-    res.send(leHotel);
+    const hotelRooms = new Hotel(Hotel.rooms); 
+    const doc = await hotelRooms.save()
+    console.log(doc)
+    res.send(hotelRooms); 
+
 });
 
 module.exports = router; 
