d3da03dc5aad5bc48333032bf24dc472fc99f1b6
avacallery <avacallery@gmail.com>
2020-03-09 14:32:58 -0400

    post route in hotels.js

--- routes/api/hotels.js
+++ routes/api/hotels.js
@@ -15,4 +15,24 @@ router.get('/', async(req, res) => {
 
 });
 
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let hotelRoom = new Hotel({
+        checkIn: req.body.checkIn,
+        checkOut: req.body.checkOut,
+        numberOfNights: req.body.numberOfNights,
+        adults: req.body.adults,
+        children: req.body.chilren,
+        rooms: req.body.rooms
+    });
+
+    hotelRoom = await hotelRoom.save();
+
+    res.send(hotelRoom);
+});
+
+
+
 module.exports = router; 
