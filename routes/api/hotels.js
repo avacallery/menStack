5822ce4e1019b94da0cfb78c28ccebbcce869233
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-03-16 13:34:06 -0400

    hotels.js file changes

--- routes/api/hotels.js
+++ routes/api/hotels.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 const { hotelSchema } = require('../../models/Hotel');
-=======
-const { Hotel, validate } = require('../../models/Hotel');
->>>>>>> 89576cb96e1ceb6b8e6d9b11a7a201783712bb07
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-<<<<<<< HEAD
 // router.get('/', (req, res) => {
 //     res.send({msg: "Works"});
 // });
@@ -30,32 +25,6 @@ router.post('/', async (req, res) => {
         // numberOfNights: req.body.numberOfNights,
         // adults: req.body.adults,
         // children: req.body.children,
-=======
-router.get('/', (req, res) => {
-    res.send({msg: "Works"});
-});
-
-// router.get('/', async(req, res) => {
-//     const hotelRooms = new Hotel(Hotel.rooms);
-//     const doc = await hotelRooms.save()
-//     console.log(doc)
-//     res.send(hotelRooms);
-//
-// });
-
-router.post('/', async (req, res) => {
-    // test array i posted was: "rooms" : "['Deluxe Suite',101]"
-    console.log(req.body)
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    let hotelRoom = new Hotel({
-        checkIn: req.body.checkIn,
-        checkOut: req.body.checkOut,
-        numberOfNights: req.body.numberOfNights,
-        adults: req.body.adults,
-        children: req.body.children,
->>>>>>> 89576cb96e1ceb6b8e6d9b11a7a201783712bb07
         rooms: req.body.rooms
     });
 
@@ -66,8 +35,4 @@ router.post('/', async (req, res) => {
 
 
 
-<<<<<<< HEAD
 module.exports = router; 
-=======
-module.exports = router; 
->>>>>>> 89576cb96e1ceb6b8e6d9b11a7a201783712bb07
