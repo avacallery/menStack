33bb892490290317804721a771016245c139bebf
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-03-17 10:51:42 -0400

    fixed spelling error in index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const app = express();
 app.use(express.json());
 
 mongoose
-  .connect(keys.mongoUri, {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log("connected to db"))
   .catch(error => console.log("DB Connection error", error));
 
 app.use(express.static("public"));
-app.use('/api/nasa', hotelRooms); 
+app.use('/api/nasa', hotelRoom); 
 
 app.listen(3000, () => console.log("listening on port 3000"));
