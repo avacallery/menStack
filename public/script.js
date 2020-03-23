725da2d19ced4cd49531739b845dbb84f122ca3e
avacallery <avacallery@gmail.com>
2020-03-23 10:23:17 -0400

    xhr GET request

--- public/script.js
+++ public/script.js
@@ -1,58 +1,32 @@
+document.getElementById("all").addEventListener("click", getData);
 
-        document.getElementById("button").addEventListener("click", getStudentData);
+function getData() {
+    const xhr = new XMLHttpRequest();
+    console.log(xhr);
 
-        function getStudentData() {
+    xhr.open('GET', 'http://localhost:3000/api/nasa', true);
+    xhr.onload = function () {
+    
+        if (xhr.status == 200) {
 
-            var xhr = new XMLHttpRequest();
-            console.log(xhr);
+            var parsedData = JSON.parse(xhr.responseText);
 
-            xhr.open('GET', 'http://localhost:5000/api/graduates/', true);
-            xhr.onload = function () {
-            
-                if (xhr.status == 200) {
+            console.log(parsedData); 
+            var dataList = ""; 
 
-                    var parsedData = JSON.parse(xhr.responseText);
-                    var dataList = ""; 
+            for (var i = 0; i < parsedData.length; i++) {
+                dataList += "<p> Check In: " + parsedData[i].checkIn + "<br>" +
+                "Check Out: " + parsedData[i].checkOut + "<br>" +
+                "Number Of Nights:" + parsedData[i].numberOfNights + "<br>" +
+                "Room Type: " + parsedData[i].roomType + "</p>"
+                +`<button id='all' onclick='dataList' value='${parsedData[i].id}'>Get All Rooms</button>`;
 
-                    for (var i = 0; i < parsedData.length; i++) {
-                        dataList += "<p> Name: " + parsedData[i].name + "<br>" +
-                        "Email: " + parsedData[i].email + "<br>" +
-                        "Date of Graduation:" + parsedData[i].dateOfGraduation + "<br>" +
-                        "Bio: " + parsedData[i].bio + "</p>"
-                        + "<button id='deleteButton' onclick='removeOneStudent'>Delete Student</button>";
-                    }
-
-                    document.getElementById("data").innerHTML = dataList 
-                } else {
-                    console.log("That is an error.");
-                }
             }
-            xhr.send();
+
+            document.getElementById("data").innerHTML = dataList; 
+        } else {
+            console.log("That is an error.");
         }
-
-        function removeOneStudent() {
-
-            var url = "http://localhost:5000/api/graduates"
-            var xhr = new XMLHttpRequest();
-            console.log(xhr); 
-
-                    // const id = document.getElementById("deleteButton").value;
-                    console.log(id);
-                    
-                    //how can we get a specific id of a graduate? 
-                    
-                     xhr.open('DELETE', url + id, true);
-                     xhr.onload = function () {
-            
-                if (xhr.status == 200) {
-
-                    var graduates = JSON.parse(xhr.responseText);
-                    console.log(graduates);
-
-                    console.log("Success.")
-                } else {
-                    console.log("That is an error.");
-                }
-            }
-            xhr.send();
-        }
+    }
+    xhr.send();
+}
